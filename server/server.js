c366760c6f4d26a7e1f437ca2f6bb57cc90c0555
EKhoroshko <xapoxa14@gmail.com>
2021-12-10 01:38:37 +0200

    test

--- server/server.js
+++ server/server.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
+const port = 3000
 
-app.listen(3000, () => {
-  console.log('Example app listening on port 3000!')
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
 })
