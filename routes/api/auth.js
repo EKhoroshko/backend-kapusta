3842b48ae954efb5a00675265d4082764404fb34
Kateryna Gurienkova <gurienkova@gmail.com>
2021-12-22 10:06:47 +0200

    varification sendGrid

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -5,8 +5,8 @@ const router = express.Router();
 const register = require("../../controllers/auth/register");
 const login = require("../../controllers/auth/login");
 const logout = require("../../controllers/auth/logout");
-// const verifyResending = require("../../controllers/auth/verifyResending");
-// const verify = require("../../controllers/auth/verify");
+const verifyResending = require("../../controllers/auth/verifyResending");
+const verify = require("../../controllers/auth/verify");
 const getCurrenUser = require("../../controllers/auth/getCurrenUser");
 const updateAvatar = require("../../controllers/auth/udateAvatar");
 const { userValidation } = require("../../midlewares/validation/user");
@@ -15,14 +15,14 @@ const upload = require("../../midlewares/auth/upload");
 const { userLogin } = require("../../midlewares/validation/userLogin");
 // const googleAuth = require("../../controllers/auth/googleAuth");
 // const googleRedirect = require("../../controllers/auth/googleRedirect");
-// const verifyValidation = require("../../midlewares/validation/verify");
+const verifyValidation = require("../../midlewares/validation/verify");
 
 router.post("/users/register", userValidation, register);
 router.post("/users/login", userLogin, login);
 router.get("/users/logout", authValidation, logout);
 
-// router.post("/users/verify", verifyValidation, verifyResending);
-// router.get("/users/verify/:verificationToken", verify);
+router.post("/users/verify", verifyValidation, verifyResending);
+router.get("/users/verify/:verificationToken", verify);
 router.get("/users/current", authValidation, getCurrenUser);
 
 router.patch(
