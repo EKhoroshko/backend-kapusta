854efe3197f97105eddf1cfcc6daba7042eb87d8
EKhoroshko <xapoxa14@gmail.com>
2022-03-13 19:45:39 +0200

    гдето ошибка

--- controllers/auth/patchPassword.js
+++ controllers/auth/patchPassword.js
@@ -3,14 +3,14 @@ const { User } = require('../../models')
 
 const patchPas = async (req, res, next) => {
   try {
-    const { id } = req.user
+    const { _id } = req.user
     const { password } = req.body
     const newPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10))
-    const findUser = await User.findById(id)
+    const findUser = await User.findById(_id)
     if (!findUser) {
       return res.status(404).json({ message: 'Not Found' })
     }
-    await User.findByIdAndUpdate(id, { password: newPassword })
+    await User.findByIdAndUpdate(_id, { password: newPassword })
     res.status(200).json({
       message: 'Password status updated'
     })
